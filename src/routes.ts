5123539806284e125b07b428a1ab33d7ffd19858
James Wright <james@jamesswright.co.uk>
2019-05-19 23:12:56 +0100

    Restructures router. Supports async routes

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { json, RouteMap } from './router.ts';
+import { apiRouter } from './api/routes.ts';
+
+const home = () => json({
+  foo: 'bar',
+  isLol: true,
+});
+
+export const routes = new RouteMap([
+  [/^\/$/, home],
+  [/^\/api\/.*$/, apiRouter],
+]);
