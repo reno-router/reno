90afb642acc1c88c233917c8305d5708f0b25bc0
James Wright <james.wright@yld.io>
2019-05-19 23:27:11 +0100

    Formats with Prettier

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,14 +2,16 @@ import { ServerRequest } from 'https://deno.land/std@v0.5/http/server.ts';
 import { createRouter, RouteMap, json } from '../router.ts';
 
 const ronSwansonQuote = async (req: ServerRequest, quotesCount = '1') => {
-  const res = await fetch(`https://ron-swanson-quotes.herokuapp.com/v2/quotes/${quotesCount}`);
+  const res = await fetch(
+    `https://ron-swanson-quotes.herokuapp.com/v2/quotes/${quotesCount}`
+  );
   const quotes = await res.json();
 
   return json(quotes);
 };
 
 const routes = new RouteMap([
-  [/\/ron-swanson-quote\/?([0-9]?)$/, ronSwansonQuote],
+  [/\/ron-swanson-quote\/?([0-9]?)$/, ronSwansonQuote]
 ]);
 
 export const apiRouter = createRouter(routes);
