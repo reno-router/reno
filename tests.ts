183fa69a2c5e008f1569428fcba07a12e336b631
James Wright <james@jamesswright.co.uk>
2019-05-21 10:27:11 +0100

    Adds first unit test

---
+++ tests.ts
@@ -0,0 +1,23 @@
+/* Right now, Deno doesn't support dynamic
+ * ESM imports which would be required to
+ * write a runner compatible with its
+ * in-built testing module. There are
+ * discussions to introduce a runner once
+ * import() is supported in the runtime.
+ *
+ * https://github.com/denoland/deno/issues/1789
+ * https://github.com/denoland/deno_std/issues/193
+ *
+ * TODO: use the provided runner once it lands
+ *
+ * Also note the _test prefix for test files. This
+ * is following the official style guide:
+ * https://deno.land/style_guide.html#eachmoduleshouldcomewithtests
+ */
+
+import { runTests } from 'https://deno.land/std@v0.5/testing/mod.ts';
+import './src/cookies_test.ts';
+
+(async () => {
+  await runTests({ parallel: true });
+})();
