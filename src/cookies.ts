35280318a6f9ff85cf83c7755cfa29f0c88beb38
James Wright <james.wright@yld.io>
2019-06-07 14:27:28 +0100

    Documents Set-Cookie header

--- src/cookies.ts
+++ src/cookies.ts
@@ -1,6 +1,18 @@
 import { setCookie } from 'https://deno.land/std@v0.7/http/cookie.ts';
 import { AugmentedResponse } from './router.ts';
 
+/* Currently, setCookie will overwrite
+ * any current Set-Cookie instances in
+ * the responses headers. This is due to
+ * the Headers API not supporting multiple
+ * entries of a single header type. Rather
+ * than work around this here, we should
+ * wait for a workaround to land in Deno.
+ * See github.com/denoland/deno_std/issues/379
+ *
+ * TODO: keep an eye on the above issue to see
+ * if and when a fix will land in Deno. Raise
+ * this as known in this repo's GitHub issues */
 export const createCookieWriter = (cookieSetter: typeof setCookie) => (
   res: AugmentedResponse,
 ) => {
