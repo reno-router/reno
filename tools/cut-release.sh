fc95746fa08beb2f3643f69575e40c59699b6a55
James Wright <james@jamesswright.co.uk>
2019-11-20 14:12:11 +0000

    Adds tooling to manage version numbers from single source truth

---
+++ tools/cut-release.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+deno --allow-read --allow-write --allow-run tools/cut-release.ts
---
+++ tools/cut-release.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env deno
+
+/* Here be dragons. Awful, hacky dragons.
+ * Rudimentary TOML parsing? ✅
+ * Creating Git tags via fs operations? ✅
+ * Don't touch this unless you know what you're doing */
+
+interface Metadata {
+  name: string;
+  description: string;
+  version: string;
+}
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+const readFileAsString = (filename: string) =>
+  decoder.decode(Deno.readFileSync(filename));
+
+const writeStringToFile = (filename: string, contents: string) =>
+  Deno.writeFileSync(filename, encoder.encode(contents));
+
+const sanitiseTOMLToken = (token: string) => token.trim().replace(/"/g, "");
+
+// TODO: use an actual TOML parser!
+const parseTOML = <TResult>(toml: string) =>
+  Object.fromEntries(
+    toml.split("\n")
+      .map(entry =>
+        entry.split("=")
+          .map(token => sanitiseTOMLToken(token))
+        )
+  ) as TResult;
+
+const tagRelease = (version: string) => {
+  const head = readFileAsString(".git/refs/heads/master");
+  writeStringToFile(`.git/refs/tags/v${version}`, head);
+};
+
+const { version } = parseTOML<Metadata>(readFileAsString("Package.toml"));
+const readmeTemplate = readFileAsString("README.template.md");
+const updatedReadme = readmeTemplate.replace(/\{\{version\}\}/g, version);
+
+writeStringToFile("README.md", updatedReadme);
+tagRelease(version);
