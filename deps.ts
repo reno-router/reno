8a33c0a3c5e0f50c20b7eeaa158e95852cb2b2e2
James Wright <james@jamesswright.co.uk>
2019-11-26 15:35:33 +0000

    Pulls Sinon type def as remote import

--- deps.ts
+++ deps.ts
@@ -1,8 +1,7 @@
 // Recommended as per https://deno.land/std/manual.md#linking-to-third-party-code
 
-// @deno-types="./types/sinon.d.ts"
 import __jsSinon from "https://cdn.pika.dev/-/sinon/7.5.0/dist-es2017/sinon.min.js";
-import Sinon from "./types/sinon.d.ts";
+import Sinon from "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/949922e0959ccea0cad7a6fa787b63b7d4e67d3f/types/sinon/index.d.ts";
 
 export * from "https://deno.land/std@v0.23.0/testing/asserts.ts";
 export * from "https://deno.land/std@v0.23.0/testing/mod.ts";
--- tools/install-types.sh
+++ tools/install-types.sh
@@ -8,4 +8,3 @@ echo "Installing type definitions..."
 
 mkdir types
 deno types > types/deno.d.ts
-curl https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/949922e0959ccea0cad7a6fa787b63b7d4e67d3f/types/sinon/index.d.ts > types/sinon.d.ts
