6b83f754d41ee5704fa1c2afa77208e26de6f5c8
James Wright <james.wright@peak.ai>
2020-06-01 12:54:01 +0100

    Migrates to jspm for Sinon in order to resolve #1

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Recommended as per https://deno.land/std/manual.md#linking-to-third-party-code
 
-import __jsSinon from "https://cdn.pika.dev/sinon@7.5.0";
+import __jsSinon from "https://dev.jspm.io/sinon@7.5.0";
 import Sinon from "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/949922e0959ccea0cad7a6fa787b63b7d4e67d3f/types/sinon/index.d.ts";
 
 export * from "https://deno.land/std@v0.51.0/testing/asserts.ts";
