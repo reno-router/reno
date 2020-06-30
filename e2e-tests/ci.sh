1ab18402819db0be84f960aed980c06c0c32b3f6
James Wright <james@jamesswright.co.uk>
2020-06-30 19:41:09 +0100

    Adds deno --version to CI entry point

--- e2e-tests/ci.sh
+++ e2e-tests/ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+deno --version
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
