46e95da2574c667a63a777ca0be37cb7804f9de6
James Wright <james@jamesswright.co.uk>
2020-06-30 18:46:12 +0100

    Writes home route test

--- e2e-tests/tests/home-route.test.ts
+++ e2e-tests/tests/home-route.test.ts
@@ -4,5 +4,11 @@ describe("/", () => {
   it("should return the expected response", () =>
     frisby.get("http://localhost:8000/")
       .expect("status", 200)
+      .expect("header", "Cache-Control", "max-age=86400")
+      .expect("header", "Set-Cookie", "requested_proto=HTTP/1.1")
+      .expect("jsonStrict", {
+        foo: "bar",
+        isLol: true,
+      })
   );
 });
