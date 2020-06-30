fe465a92348f6035eabaae043e90131acebf366b
James Wright <james@jamesswright.co.uk>
2020-06-30 18:54:10 +0100

    Adds E2E tests for /api/json-body

---
+++ e2e-tests/tests/api-route.test.ts
@@ -0,0 +1,29 @@
+import frisby from "frisby";
+
+describe("/api", () => {
+  describe("/json-body", () => {
+    it("should return the body passed via the request", () =>
+      frisby.post("http://localhost:8000/api/json-body", {
+        name: "James",
+        age: 30,
+      })
+        .expect("status", 200)
+        .expect("header", "Content-Type", "application/json")
+        .expect("jsonStrict", {
+          message: "Here's the body you posted to this endpoint",
+          name: "James",
+          age: 30,
+        })
+    );
+
+    it("should return HTTP 405 when a non-POST method is used", () =>
+      frisby.put("http://localhost:8000/api/json-body", {
+        name: "James",
+        age: 30,
+      })
+        .expect("status", 405)
+        .expect("header", "Content-Type", "text/plain")
+        .expect("bodyContains", "Method PUT not allowed for /api/json-body")
+    );
+  });
+});
