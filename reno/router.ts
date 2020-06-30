8a7ad132004c66c70df44ba21446e880100a973b
James Wright <james@jamesswright.co.uk>
2020-06-30 16:16:05 +0100

    Adds documentation for public router functions

--- reno/router.ts
+++ reno/router.ts
@@ -41,6 +41,27 @@ export type Router = (routes: RouteMap) => RouteParser;
 export type RouteMap = Map<RegExp | string, RouteHandler>;
 export class NotFoundError extends Error {} // TODO: rename RouteMissingError?
 
+/**
+ * Creates a `RouteMap`, a `Map` that holds route handling functions
+ * and keys them by the path by which the router will make them
+ * accessible:
+ * ```ts
+ * export const routes = createRouteMap([
+ *   ["/home", () => textResponse("Hello world!")],
+ *
+ *   // Supports RegExp routes for further granularity
+ *   [/^\/api\/swanson\/?([0-9]?)$/, async (req: AugmentedRequest) => {
+ *     const [quotesCount = "1"] = req.routeParams;
+ *
+ *     const res = await fetch(
+ *       `https://ron-swanson-quotes.herokuapp.com/v2/quotes/${quotesCount}`,
+ *    );
+ *
+ *     return jsonResponse(await res.json());
+ *   }],
+ * ]);
+ * ```
+ */
 export function createRouteMap(routes: [RegExp | string, RouteHandler][]) {
   return new Map(routes);
 }
@@ -98,8 +119,45 @@ export function routerCreator(
     };
 }
 
-/* We could just do `const createRouter = routerCreator(parsePath, writeCookies)`
- * here, but sadly deno doc isn't able to surface it as a function :( */
+/**
+ * Creates a Reno router for the given routes, which
+ * can then be invoked as an async function when
+ * Deno's HTTP server receives a request:
+ *
+ * ```ts
+ * import {
+ *   ServerRequest,
+ *   listenAndServe,
+ * } from "https://deno.land/std@v0.58.0/http/server.ts";
+
+ * import { createRouter } from "https://deno.land/x/reno@<VERSION>/reno/mod.ts";
+ * import { routes } from "./routes.ts";
+ *
+ * const BINDING = ":8000";
+ *
+ * const router = createRouter(routes);
+ *
+ * (async () => {
+ *   console.log(`Listening for requests on ${BINDING}...`);
+ *
+ *   await listenAndServe(
+ *     BINDING,
+ *     async (req: ServerRequest) => {
+ *       logRequest(req);
+ *
+ *       try {
+ *         const res = await router(req);
+ *         return req.respond(res);
+ *       } catch (e) {
+ *         return req.respond(mapToErrorResponse(e));
+ *       }
+ *     },
+ *   );
+ * })();
+ * ```
+ */
 export function createRouter(routes: RouteMap) {
+  /* We could just do `const createRouter = routerCreator(parsePath, writeCookies)`
+   * here, but sadly deno doc isn't able to surface it as a function :( */
   return routerCreator(parsePath, writeCookies)(routes);
 }
