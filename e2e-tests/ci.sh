2dc8796f6db31f06ba9acef8d57b9df3c76f1b59
James Wright <james@jamesswright.co.uk>
2020-06-30 19:32:16 +0100

    Invokes nvm initialisation directly in CI script

--- e2e-tests/ci.sh
+++ e2e-tests/ci.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use
 npm i
 npm test
