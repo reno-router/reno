a7d716b8dcfebd9d9b983879ffa3f0716155fb75
James Wright <james@jamesswright.co.uk>
2020-10-17 17:28:06 +0100

    Fixes linting issue

--- reno/router.ts
+++ reno/router.ts
@@ -136,7 +136,7 @@ export function routerCreator(
       const queryParams = rootQueryParams || url.searchParams;
 
       // TODO: restructure this lookup to support O(1) retrieval
-      for (let [path, handler] of routes) {
+      for (const [path, handler] of routes) {
         const [firstMatch, ...restMatches] =
           url.pathname.match(pathParser(path)) || [];
 
