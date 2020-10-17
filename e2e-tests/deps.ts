2f3921f4a387d85c581c95c6f3ec14ec8c7d6cb9
James Wright <james@jamesswright.co.uk>
2020-10-17 16:29:54 +0100

    Migrates methods tests to SuperDeno

--- e2e-tests/deps.ts
+++ e2e-tests/deps.ts
@@ -1,1 +1,1 @@
-export { superdeno } from "https://deno.land/x/superdeno@2.3.2/mod.ts";
+export { superdeno, SuperDeno } from "https://deno.land/x/superdeno@2.3.2/mod.ts";
---
+++ e2e-tests/tests/methods_route_test.ts
@@ -0,0 +1,22 @@
+import { superdeno, SuperDeno } from "../deps.ts";
+import app from "../../example/app.ts";
+
+["GET", "POST"].forEach((method) => {
+  Deno.test(`/methods-endpoint should allow HTTP ${method}`, async () => {
+    await superdeno(app)[method.toLowerCase() as keyof SuperDeno]("/methods/endpoint")
+      .expect(200)
+      .expect("Content-Type", "text/plain")
+      .expect(`You performed a HTTP ${method}!`);
+  });
+});
+
+["PATCH", "PUT", "DELETE", "OPTIONS"].forEach((method) => {
+  Deno.test(`/methods-endpoint should not allow HTTP ${method}`, async () => {
+    await superdeno(app)[method.toLowerCase() as keyof SuperDeno]("/methods/endpoint")
+      .expect(405)
+      .expect("Content-Type", "text/plain")
+      .expect(
+        `Method ${method} not allowed for /methods/endpoint`,
+      );
+  });
+});
