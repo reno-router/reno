acafbdf51e8bed4e64e541cd519bf136975babc9
James Wright <james@jamesswright.co.uk>
2021-07-30 11:09:25 +0100

    Refines TODO

--- reno/router.ts
+++ reno/router.ts
@@ -135,7 +135,8 @@ export function routerCreator(
       );
       const queryParams = rootQueryParams || url.searchParams;
 
-      // TODO: restructure this lookup to support O(1) retrieval
+      /* TODO: restructure this lookup to support O(1) retrieval.
+       * Perhaps compute a radix tree or a similar structure. */
       for (const [path, handler] of routes) {
         const [firstMatch, ...restMatches] =
           url.pathname.match(pathParser(path)) || [];
