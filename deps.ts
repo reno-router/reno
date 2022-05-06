a76616357fa917fdf43f24ae0876786540f32e12
James Wright <hi@james.engineering>
2022-05-06 02:10:28 +0100

    [Reno Buildbot] Upgrades Deno to 1.21.2 and std to 0.138.0 (#54)
    Co-authored-by: jamesseanwright <jamesseanwright@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Recommended as per https://deno.land/std/manual.md#linking-to-third-party-code
 
-export * from "https://deno.land/std@0.137.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.137.0/io/mod.ts";
-export * from "https://deno.land/std@0.137.0/http/cookie.ts";
+export * from "https://deno.land/std@0.138.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.138.0/io/mod.ts";
+export * from "https://deno.land/std@0.138.0/http/cookie.ts";
 
 import __jsTestDouble from "https://dev.jspm.io/testdouble@3.16.0";
 import * as TestDouble from "https://raw.githubusercontent.com/testdouble/testdouble.js/ecd90efe4649b287c33831a7b94a8a5eb96b8ed0/index.d.ts";
--- example/api/routes.ts
+++ example/api/routes.ts
@@ -1,4 +1,4 @@
-import { StringReader } from "https://deno.land/std@0.137.0/io/readers.ts";
+import { StringReader } from "https://deno.land/std@0.138.0/io/readers.ts";
 
 import colossalData from "./colossal.ts";
 
