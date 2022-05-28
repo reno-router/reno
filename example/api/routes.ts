de180ac97175e8c254d07818c8789a72f6931332
James Wright <hi@james.engineering>
2022-05-28 02:16:05 +0100

    [Reno Buildbot] Upgrades Deno to 1.22.1 and std to 0.141.0 (#57)
    Co-authored-by: jamesseanwright <jamesseanwright@users.noreply.github.com>

--- example/api/routes.ts
+++ example/api/routes.ts
@@ -1,4 +1,4 @@
-import { StringReader } from "https://deno.land/std@0.140.0/io/readers.ts";
+import { StringReader } from "https://deno.land/std@0.141.0/io/readers.ts";
 
 import colossalData from "./colossal.ts";
 
