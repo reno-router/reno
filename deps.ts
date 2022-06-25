2155da5e1960230d5adc11b928ea62f47820a12a
jamesseanwright <jamesseanwright@users.noreply.github.com>
2022-06-25 01:20:49 +0000

    [Reno Buildbot] Upgrades Deno to 1.23.1 and std to 0.145.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Recommended as per https://deno.land/std/manual.md#linking-to-third-party-code
 
-export * from "https://deno.land/std@0.144.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.144.0/io/mod.ts";
-export * from "https://deno.land/std@0.144.0/http/cookie.ts";
+export * from "https://deno.land/std@0.145.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.145.0/io/mod.ts";
+export * from "https://deno.land/std@0.145.0/http/cookie.ts";
 
 export * as sinon from "https://cdn.skypack.dev/sinon@v14.0.0";
