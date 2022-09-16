86c855d6c8007a119ca5c914dd612e386234a81e
jamesseanwright <jamesseanwright@users.noreply.github.com>
2022-09-16 01:51:08 +0000

    [Reno Buildbot] Upgrades Deno to 1.25.3 and std to 0.156.0

--- example/api/routes.ts
+++ example/api/routes.ts
@@ -1,4 +1,4 @@
-import { StringReader } from "https://deno.land/std@0.155.0/io/readers.ts";
+import { StringReader } from "https://deno.land/std@0.156.0/io/readers.ts";
 
 import colossalData from "./colossal.ts";
 
--- example/server.ts
+++ example/server.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
 
 import app from "./app.ts";
 
