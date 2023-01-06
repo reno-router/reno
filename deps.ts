8305509d53dc3b62d924d7e9e5aa0f4024b64dc4
jamesseanwright <jamesseanwright@users.noreply.github.com>
2023-01-06 09:48:46 +0000

    [Reno Buildbot] Upgrades Deno to 1.29.2 and std to 0.171.0

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Recommended as per https://deno.land/std/manual.md#linking-to-third-party-code
 
-export * from "https://deno.land/std@0.170.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.170.0/io/mod.ts";
-export * from "https://deno.land/std@0.170.0/http/cookie.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.170.0/streams/conversion.ts"
+export * from "https://deno.land/std@0.171.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.171.0/io/mod.ts";
+export * from "https://deno.land/std@0.171.0/http/cookie.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.171.0/streams/conversion.ts"
 
 export * as sinon from "https://cdn.skypack.dev/sinon@v14.0.0";
