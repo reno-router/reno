2847b3565d9fe45d96b123a52245e3e5ff846245
James Wright <hi@james.engineering>
2023-02-06 09:54:30 +0000

    Updates StringReader import location

--- example/api/routes.ts
+++ example/api/routes.ts
@@ -1,4 +1,4 @@
-import { StringReader } from "https://deno.land/std@0.176.0/io/readers.ts";
+import { StringReader } from "https://deno.land/std@0.176.0/io/string_reader.ts";
 
 import colossalData from "./colossal.ts";
 
